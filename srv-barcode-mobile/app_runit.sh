3cfae1e1de2759ac82de4cfb818909cb60c9d69b
TM <tm@macadmin.ru>
2016-04-19 16:12:05 +0300

    run app as non root user

--- srv-barcode-mobile/app_runit.sh
+++ srv-barcode-mobile/app_runit.sh
@@ -4,6 +4,9 @@
 
 set -e
 
+addgroup -g $GROUP_ID appuser
+adduser -h /app -s /sbin/nologin -D -u $USER_ID -G appuser appuser
+
 export HOME=/app
 
 BIN="java -jar ${HOME}/app.jar -S rackup -o 0.0.0.0 -p 3000"
@@ -13,6 +16,8 @@ CT="/usr/local/bin/consul-template"
 CTCFG="/etc/consul-template"
 $CT -consul $EXTERNAL_IP:8500 -template "$CTCFG/app.ctmpl:$CFG" -once
 
+chown -R appuser:appuser ${HOME}
+
 cd ${HOME}
 exec 2>&1
-exec ${BIN}
+exec chpst -uappuser ${BIN}
